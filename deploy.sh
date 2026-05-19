6921274f7ae82945a80fdccd3e2cace550a41837
Yesuraju-1234 <vallepuyesuraju11@gmail.com>
2026-05-19 21:13:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ if [ $node_app=='nodeapp' ]; then
         docker rm -f nodeapp
 fi
 <rajajjjjjjjjjj>
+<demo gitgub>
 images=`docker images | grep kammana/nodejenkins | awk '{print $3}'`
 docker rmi $images
 docker run -d -p 8080:8080 --name nodeapp $1
