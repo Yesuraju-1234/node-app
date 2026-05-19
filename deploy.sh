30e2a2bd44bdd2704638c412eed94ee22d0a882a
Yesuraju-1234 <vallepuyesuraju11@gmail.com>
2026-05-19 17:39:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ $node_app=='nodeapp' ]; then
     echo "nodeapp is running, lets delete"
         docker rm -f nodeapp
 fi
-
+<raja yesu raja>
 images=`docker images | grep kammana/nodejenkins | awk '{print $3}'`
 docker rmi $images
 docker run -d -p 8080:8080 --name nodeapp $1
